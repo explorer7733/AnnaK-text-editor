5736b15766af49b6a405afd123d34209df59e8e1
explorer1500 <163674076+explorer7733@users.noreply.github.com>
2024-08-17 15:34:28 -0700

    debug the code, fix it

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,10 +29,10 @@ export const getDb = async () => {console.log('GET from the database');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
-const request = store.getAll();
+const request = store.get(1);
 const result = await request;
 console.log('result.value', result);
-return result;
+return result?.value;
 };
 
 initdb();
